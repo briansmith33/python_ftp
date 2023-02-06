30bbdf7317a1cb5cdf4c115907e044252bcc3695
Brian <Brianmatthewsmith33@outlook.com>
2023-02-05 19:31:21 -0800

    first commit

---
+++ client.py
@@ -0,0 +1,80 @@
+import socket
+import time
+import sys
+
+
+class FTPClient:
+    def __init__(self,
+                 host=socket.gethostbyname(socket.gethostname()),
+                 port=21,
+                 buffer_size=1024):
+        self.host = host
+        self.port = port
+        self.addr = (host, port)
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.buffer_size = buffer_size
+
+    def connect(self):
+        self.client.connect(self.addr)
+        greeting = ''
+        done = False
+        while not done:
+            greeting += self.client.recv(self.buffer_size).decode()
+            for line in greeting.splitlines():
+                if line.startswith('530'):
+                    done = True
+                    break
+        print(greeting)
+        username = input(f'User ({self.host}:{self.port}): ')
+        self.client.send(b'USER ' + username.encode())
+        response = self.client.recv(self.buffer_size).decode().strip()
+        print(response)
+        if not response.startswith('331'):
+            return False
+        password = input('Password: ')
+        self.client.send(b'PASS '+password.encode())
+        response = self.client.recv(self.buffer_size).decode().strip()
+        print(response)
+        if not response.startswith('230'):
+            return False
+        return True
+
+    def run(self):
+        if self.connect():
+            while True:
+                command = input('ftp> ')
+                if command.lower() == 'quit':
+                    self.client.send(b'QUIT')
+                    response = self.client.recv(self.buffer_size).decode().strip()
+                    print(response)
+                    break
+
+                if command.startswith('ls '):
+                    self.client.send(b'LIST '+command[len('ls '):].encode()+b'\r\n')
+                    transferring = False
+                    transfer_start = None
+                    size_transferred = 0
+                    while True:
+                        response = self.client.recv(self.buffer_size).decode().strip()
+                        print(response)
+
+                        if response.startswith('125'):
+                            transferring = True
+                            transfer_start = time.time()
+                        if response.startswith('226'):
+                            print(str(size_transferred) + ' bytes received in ' + str(time.time() - transfer_start) + ' seconds')
+                            break
+
+                        if transferring:
+                            size_transferred += sys.getsizeof(response)
+
+                if command.startswith('pwd'):
+                    self.client.send(b'PWD \r\n')
+                    response = self.client.recv(self.buffer_size).decode().strip()
+                    print(response)
+
+        self.client.close()
+
+
+if __name__ == "__main__":
+    FTPClient().run()
---
+++ server.py
@@ -0,0 +1,102 @@
+from threading import Thread
+import socket
+import os
+
+
+class FTPServer:
+    def __init__(self,
+                 host=socket.gethostbyname(socket.gethostname()),
+                 port=21,
+                 root='./test',
+                 allows_anonymous=True,
+                 buffer_size=1024):
+        self.host = host
+        self.port = port
+        self.addr = (host, port)
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.root = root
+        self.cwd = root
+        self.allows_anonymous = allows_anonymous
+        self.buffer_size = buffer_size
+        self.users = {'brian': 'password'}
+
+    def get_username(self, conn):
+        while True:
+            username = conn.recv(self.buffer_size).decode().strip()
+            if username.upper().startswith('USER'):
+                username = username[len('USER '):]
+                print(username)
+                if username == 'anonymous' and self.allows_anonymous:
+                    conn.send(b'331 Guest login ok, leave password blank.\r\n')
+                    break
+                elif username in self.users.keys():
+                    conn.send(b'331 User name ok, need password.\r\n')
+                    break
+            conn.send(b'430 Invalid username\r\n')
+
+        return username
+
+    def authenticate(self, conn, user):
+        while True:
+            password = conn.recv(self.buffer_size).decode().strip()
+            if password.upper().startswith('PASS'):
+                password = password[len('PASS '):]
+                if password == 'QUIT':
+                    return False
+                if user == 'anonymous':
+                    conn.send(b'230 Anonymous login ok, access restrictions apply\r\n')
+                    return True
+                elif self.users[user] == password:
+                    conn.send(b'230 User logged in\r\n')
+                    return True
+            conn.send(b'430 Invalid password\r\n')
+
+    def accept_connection(self, conn, addr):
+        conn.send(b'Connected to '+self.host.encode()+b'\r\n')
+        conn.send(b'220 Nexus FTP Server\r\n')
+        conn.send(b'530 Please login with USER and PASS\r\n')
+        username = self.get_username(conn)
+        if self.authenticate(conn, username):
+            while True:
+                command = conn.recv(self.buffer_size).decode().strip()
+                if command.upper().startswith('QUIT'):
+                    conn.send(b'231 User logged out, service terminated')
+                    break
+
+                if command.upper().startswith('LIST'):
+                    directory = self.root+'/'+command[len('LIST '):]
+                    conn.send(b'200 PORT OK\r\n')
+                    conn.send(b'125 Data connection already open, starting transfer\r\n')
+                    if os.path.exists(directory):
+                        if directory.lower().startswith('c:'):
+                            if self.root.startswith('.'):
+                                full_path = os.getcwd()+self.root[1:]
+                            else:
+                                full_path = self.root
+                            if full_path in directory:
+                                conn.send('\n'.join(os.listdir(directory)).encode())
+                            else:
+                                conn.send(b'550 No such file or directory\r\n')
+                    else:
+                        conn.send(b'550 No such file or directory\r\n')
+                    conn.send(b'226 Transfer complete\r\n')
+
+                if command.upper().startswith('PWD'):
+                    conn.send(b'200 '+self.cwd.encode()+b'\r\n')
+
+        conn.close()
+
+    def run(self):
+        self.server.bind(self.addr)
+        self.server.listen(2)
+        while True:
+            try:
+                conn, addr = self.server.accept()
+                Thread(target=self.accept_connection, args=(conn, addr)).start()
+            except KeyboardInterrupt:
+                break
+
+
+if __name__ == '__main__':
+    server = FTPServer()
+    server.run()
